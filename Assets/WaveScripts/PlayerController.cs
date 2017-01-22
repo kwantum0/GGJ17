375c898ff2c2e25082ff506f0aab1465ea4ae14c
Daniel <Daniel>
2017-01-21 22:33:55 -0500

    waves now have wavey widths. Wave ception

--- Assets/WaveScripts/PlayerController.cs
+++ Assets/WaveScripts/PlayerController.cs
@@ -8,11 +8,23 @@ public class PlayerController : MonoBehaviour {
 	public const int DEFAULT_START_HEALTH = 6;
 	[Range(0,9)]
 	public const int DEFAULT_START_ENERGY = 0;
+	[Range(0f,1f)]
+	public const float FREQUENCY_STEP = 0.2f;
+	[Range(0f,3f)]
+	public const float STEP_SECONDS = 1f;
+	private float nextPlayerOneStepTime = -1f;
+	private float nextPlayerTwoStepTime = -1f;
+
 
 	public int PlayerOneHealth;
 	public int PlayerTwoHealth;
 	public int PlayerOneEnergy;
 	public int PlayerTwoEnergy;
+
+	public float PlayerOneStartFrequency;
+	public float PlayerOneEndFrequency;
+	public float PlayerTwoStartFrequency;
+	public float PlayerTwoEndFrequency;
 
 
 	// Use this for initialization
@@ -22,7 +34,81 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		
+		/*if (Input.GetKey (KeyCode.A)) {
+			PlayerOneFrequency += FREQUENCY_STEP * Time.smoothDeltaTime;
+		} else {
+			PlayerOneFrequency -= FREQUENCY_STEP * Time.smoothDeltaTime;
+		}
+		PlayerOneFrequency = Mathf.Max (PlayerOneFrequency, 0);
+		PlayerOneFrequency = Mathf.Min (PlayerOneFrequency, 1);
+
+		if (Input.GetKey(KeyCode.Semicolon)) {
+			PlayerTwoFrequency += FREQUENCY_STEP * Time.smoothDeltaTime;
+		} else {
+			PlayerTwoFrequency -= FREQUENCY_STEP * Time.smoothDeltaTime;
+		}
+		PlayerTwoFrequency = Mathf.Max (PlayerTwoFrequency, 0);
+		PlayerTwoFrequency = Mathf.Min (PlayerTwoFrequency, 1);
+
+		WaveController.setFrequencies (PlayerOneFrequency, PlayerTwoFrequency);
+		*/
+
+		if (Input.GetKeyDown (KeyCode.A) && Time.fixedTime > nextPlayerOneStepTime) {
+			PlayerOneStartFrequency = PlayerOneEndFrequency;
+			PlayerOneEndFrequency += FREQUENCY_STEP;
+
+			if(PlayerOneEndFrequency < 1f){
+				nextPlayerOneStepTime = Time.fixedTime + STEP_SECONDS;
+			}
+
+			PlayerOneEndFrequency = Mathf.Min (PlayerOneEndFrequency, 1f);
+
+		} else if (Input.GetKeyDown (KeyCode.S) && Time.fixedTime > nextPlayerOneStepTime) {
+			PlayerOneStartFrequency = PlayerOneEndFrequency;
+			PlayerOneEndFrequency -= FREQUENCY_STEP;
+
+			if(PlayerOneEndFrequency > 0f){
+				nextPlayerOneStepTime = Time.fixedTime + STEP_SECONDS;
+			}
+
+			PlayerOneEndFrequency = Mathf.Max (PlayerOneEndFrequency, 0f);
+		}
+
+		if (Time.fixedTime < nextPlayerOneStepTime) {
+			float percent = (nextPlayerOneStepTime - Time.fixedTime) / STEP_SECONDS;
+			float value = Mathf.Lerp (PlayerOneEndFrequency, PlayerOneStartFrequency, percent);
+			WaveController.setFrequencies ("one", value);
+			WaveController.setWidth ("one", percent);
+		}
+
+		if (Input.GetKeyDown (KeyCode.L) && Time.fixedTime > nextPlayerTwoStepTime) {
+			PlayerTwoStartFrequency = PlayerTwoEndFrequency;
+			PlayerTwoEndFrequency += FREQUENCY_STEP;
+
+			if(PlayerTwoEndFrequency < 1f){
+				nextPlayerTwoStepTime = Time.fixedTime + STEP_SECONDS;
+			}
+
+			PlayerTwoEndFrequency = Mathf.Min (PlayerTwoEndFrequency, 1f);
+
+		} else if (Input.GetKeyDown(KeyCode.Semicolon) && Time.fixedTime > nextPlayerTwoStepTime) {
+			PlayerTwoStartFrequency = PlayerTwoEndFrequency;
+			PlayerTwoEndFrequency -= FREQUENCY_STEP;
+
+			if(PlayerTwoEndFrequency > 0f){
+				nextPlayerTwoStepTime = Time.fixedTime + STEP_SECONDS;
+			}
+
+			PlayerTwoEndFrequency = Mathf.Max (PlayerTwoEndFrequency, 0f);
+		}
+
+		if (Time.fixedTime < nextPlayerTwoStepTime) {
+			float percent = (nextPlayerTwoStepTime - Time.fixedTime) / STEP_SECONDS;
+			float value = Mathf.Lerp (PlayerTwoEndFrequency, PlayerTwoStartFrequency, percent);
+			WaveController.setFrequencies ("two", value);
+			WaveController.setWidth ("two", percent);
+		}
+			
 	}
 
 	void newGame() {
@@ -31,8 +117,10 @@ public class PlayerController : MonoBehaviour {
 		PlayerTwoHealth = DEFAULT_START_HEALTH;
 		PlayerOneEnergy = DEFAULT_START_ENERGY;
 		PlayerTwoEnergy = DEFAULT_START_ENERGY;
-
-
-
+		PlayerOneStartFrequency = 0f;
+		PlayerOneEndFrequency = 0f;
+		PlayerTwoStartFrequency = 0f;
+		PlayerTwoEndFrequency = 0f;
+		WaveController.setFrequencies (0f, 0f);
 	}
 }
--- Assets/WaveScripts/WaveController.cs
+++ Assets/WaveScripts/WaveController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class WaveController : MonoBehaviour {
 
+	private static WaveController _instance;
+
+	public const float MAX_FREQUENCY = 12f;
+	public const float MIN_FREQUENCY = 2f;
+	public const float WIDTH_MIN = 0.15f;
+	public const float WIDTH_MAX = 0.75f;
+
 	private LineRenderer line;
 	private List<GameObject> colliders;
 	private List<Vector3> points;
@@ -18,9 +25,9 @@ public class WaveController : MonoBehaviour {
 
 	[Range(-0.5f,0.5f)]
 	public float colliderOffset = 0.1f;
-	[Range(2f,10f)]
+	[Range(2f,12f)]
 	public float frequencyLeft = 2f;
-	[Range(2f,10f)]
+	[Range(2f,12f)]
 	public float frequencyRight = 2f;
 	[Range(0f,4f)]
 	public float amplitude = 1;
@@ -33,8 +40,13 @@ public class WaveController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		if (_instance == null) {
+			_instance = this;
+		}
+
 		line = GetComponent<LineRenderer> ();
 		strokeWidth = line.startWidth;
+		resetWidths ();
 		colliders = new List<GameObject>();
 		points = new List<Vector3> ();
 		start = new Vector3 (startPosition.transform.position.x, startPosition.transform.position.y, 0);
@@ -65,7 +77,6 @@ public class WaveController : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
-
 		// UPDATE THE LINE
 		for (int i = 0; i < points.Count; i++) {
 			float bevel = alwaysBevel.Evaluate ((float)i / (float)points.Count);
@@ -80,10 +91,11 @@ public class WaveController : MonoBehaviour {
 
 		// UPDATE THE BOX COLLIDERS - COMPLETE
 		for (int i = 1; i < points.Count; i++) {
+			float StrokePercent = line.widthCurve.Evaluate((float)i / (float)points.Count);
 			BoxCollider2D current = colliders[i - 1].GetComponent<BoxCollider2D>();
 			Vector3 point1 = points [i - 1];
 			Vector3 point2 = points [i];
-			current.size = new Vector2 (Vector3.Distance(point1, point2) + colliderOffset, strokeWidth + colliderOffset);
+			current.size = new Vector2 (Vector3.Distance(point1, point2) + colliderOffset, StrokePercent + colliderOffset);
 			current.transform.position = Vector3.Lerp (point1, point2, 0.5f);
 			Vector3 direction = point1 - point2;
 			float angle = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
@@ -91,12 +103,62 @@ public class WaveController : MonoBehaviour {
 		}
 	}
 
+	public static void setFrequencies(float player1, float player2){
+		if (_instance != null) {
+			_instance.frequencyLeft = Mathf.Lerp (MIN_FREQUENCY, MAX_FREQUENCY, player1);
+			_instance.frequencyRight = Mathf.Lerp (MIN_FREQUENCY, MAX_FREQUENCY, player2);
+		}
+	}
+	public static void setFrequencies(string i, float player){
+		if (_instance != null) {
+			if (i == "one") {
+				_instance.frequencyLeft = Mathf.Lerp (MIN_FREQUENCY, MAX_FREQUENCY, player);
+			} else {
+				_instance.frequencyRight = Mathf.Lerp (MIN_FREQUENCY, MAX_FREQUENCY, player);
+			}
+		}
+	}
+
+	public void resetWidths(){
+		List<Keyframe> keys = new List<Keyframe> (4);
+		keys.Insert(0, new Keyframe (0f, WIDTH_MIN, 0f, 0f));
+		keys.Insert(1, new Keyframe (0.15f, WIDTH_MIN, 0f, 0f));
+		keys.Insert(2, new Keyframe (0.85f, WIDTH_MIN, 0f, 0f));
+		keys.Insert(3, new Keyframe (1f, WIDTH_MIN, 0f, 0f));
+		line.widthCurve = new AnimationCurve (keys.ToArray());
+	}
+
+	public static void setWidth(string i, float percent){
+		if (_instance != null) {
+			AnimationCurve current = _instance.line.widthCurve;
+			List<Keyframe> newKeys = new List<Keyframe> (current.keys.Length);
+			float value = Mathf.Lerp (WIDTH_MIN, WIDTH_MAX, percent);
+			int index = (i == "one" ? 0 : 3);
+
+			for(int j = 0; j < current.keys.Length; j++){
+				if(index == j){
+					newKeys.Insert(j, new Keyframe(current.keys[j].time, value));
+				} else {
+					newKeys.Insert(j, current.keys[j]);
+				}
+			}
+
+			// current = new Keyframe(current.keys[index].time, Mathf.Lerp (WIDTH_MIN, WIDTH_MAX, percent));
+			//_instance.line.widthCurve.SmoothTangents (0, 1);
+
+			_instance.line.widthCurve = new AnimationCurve(newKeys.ToArray());
+			_instance.line.widthCurve.SmoothTangents (0, 0.5f);
+
+		}
+
+	}
+
 	// ALL THE MATH!
 	private float CalcRightWave(float x){
-		return Mathf.Sin(frequencyRight*(x - Mathf.PI) + Time.fixedTime * speed);
+		return Mathf.Sin(frequencyRight*(x + Mathf.PI) + Time.fixedTime * speed);
 	}
 	private float CalcLeftWave(float x){
-		return Mathf.Sin(frequencyLeft*(x + Mathf.PI) - Time.fixedTime * speed);
+		return Mathf.Sin(frequencyLeft*(x - Mathf.PI) - Time.fixedTime * speed);
 	}
 		
 }
