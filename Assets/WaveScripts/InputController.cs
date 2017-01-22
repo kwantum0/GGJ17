48a8a927cc18c266d191d032a26292a20db6f3fa
Daniel <Daniel>
2017-01-22 12:20:51 -0500

    Added ability to respawn bomb, anded random noise generator

---
+++ Assets/WaveScripts/InputController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+	// KEY MAPPINGS
+	public KeyCode[] PlayerOneGuitar;
+	public KeyCode PlayerOneAttack;
+	public KeyCode[] PlayerTwoGuitar;
+	public KeyCode PlayerTwoAttack;
+
+	public string RandomSeed;
+	public int NoiseSize = 100;
+	public NoteType[] Notes;
+
+	private float[] PlayerOneNoise;
+	private float[] PlayerTwoNoise;
+
+	public int[] PlayerOneSequence;
+	public int[] PlayerTwoSequence;
+
+	// Use this for initialization
+	void Awake () {
+		generateRandomNoise ();
+		generateSequences();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void generateRandomNoise(){
+		Random.seed = RandomSeed.GetHashCode ();
+		PlayerOneNoise = new float[NoiseSize];
+		PlayerTwoNoise = new float[NoiseSize];
+		int i = 0;
+		while (i < NoiseSize) {
+			PlayerOneNoise[i] = Random.value;
+			PlayerTwoNoise[i] = Random.value;
+			i++;
+		}
+	}
+
+	void generateSequences() {
+		int i = 0;
+		while (i < NoiseSize) {
+			PlayerOneNoise[i]
+			i++;
+		}
+	}
+
+	[System.Serializable]
+	public struct NoteType {
+		public int id;
+		public Color color;
+		[Range(0f,1f)]
+		public float minNoiseIncl;
+		[Range(0f,1f)]
+		public float maxNoiseExcl;
+	}
+}
--- Assets/WaveScripts/PlayerController.cs
+++ Assets/WaveScripts/PlayerController.cs
@@ -11,12 +11,14 @@ public class PlayerController : MonoBehaviour {
 	[Range(0f,1f)]
 	public const float DEFAULT_START_FREQUENCY = 0f;
 	[Range(0f,1f)]
-	public const float FREQUENCY_STEP = 0.2f;
+	public const float FREQUENCY_STEP = 0.1f;
 	[Range(0f,3f)]
-	public const float STEP_SECONDS = 1f;
+	public const float STEP_SECONDS = 0.5f;
 	private float nextPlayerOneStepTime = -1f;
 	private float nextPlayerTwoStepTime = -1f;
 
+	public int PlayerOneScore;
+	public int PlayerTwoScore;
 
 	public int PlayerOneHealth;
 	public int PlayerTwoHealth;
@@ -54,22 +56,18 @@ public class PlayerController : MonoBehaviour {
 
 		WaveController.setFrequencies (PlayerOneFrequency, PlayerTwoFrequency);
 		*/
-
+		// SET BOMB 
+		if (Input.GetKeyDown (KeyCode.Y) && Debug.isDebugBuild) {
+			respawnBomb ();
+		}
 		// PLAYER ONE
-		if (Input.GetKeyDown (KeyCode.A) && Time.fixedTime > nextPlayerOneStepTime) {
-			PlayerOneStartFrequency = PlayerOneEndFrequency;
-			PlayerOneEndFrequency += FREQUENCY_STEP;
-			nextPlayerOneStepTime = Time.fixedTime + STEP_SECONDS;
-			PlayerOneEndFrequency = Mathf.Min (PlayerOneEndFrequency, 1f);
-
-		} else if (Input.GetKeyDown (KeyCode.S) && Time.fixedTime > nextPlayerOneStepTime) {
-			PlayerOneStartFrequency = PlayerOneEndFrequency;
-			PlayerOneEndFrequency -= FREQUENCY_STEP;
-			nextPlayerOneStepTime = Time.fixedTime + STEP_SECONDS;
-			PlayerOneEndFrequency = Mathf.Max (PlayerOneEndFrequency, 0f);
-
+		if (Input.GetKeyDown (KeyCode.A)) {
+			IncreaseFrequencyStateP1 ();
+		} else if (Input.GetKeyDown (KeyCode.S)) {
+			DecreaseFrequencyStateP1 ();
 		}
 
+		// WAVE ANIMTION - PLAYER ONE
 		if (Time.fixedTime < nextPlayerOneStepTime  && PlayerOneStartFrequency != PlayerOneEndFrequency) {
 			float percent = (nextPlayerOneStepTime - Time.fixedTime) / STEP_SECONDS;
 			float value = Mathf.Lerp (PlayerOneEndFrequency, PlayerOneStartFrequency, percent);
@@ -78,18 +76,13 @@ public class PlayerController : MonoBehaviour {
 		}
 
 		// PLAYER TWO
-		if (Input.GetKeyDown (KeyCode.L) && Time.fixedTime > nextPlayerTwoStepTime) {
-			PlayerTwoStartFrequency = PlayerTwoEndFrequency;
-			PlayerTwoEndFrequency += FREQUENCY_STEP;
-			PlayerTwoEndFrequency = Mathf.Min (PlayerTwoEndFrequency, 1f);
-			nextPlayerTwoStepTime = Time.fixedTime + STEP_SECONDS;
-		} else if (Input.GetKeyDown(KeyCode.Semicolon) && Time.fixedTime > nextPlayerTwoStepTime) {
-			PlayerTwoStartFrequency = PlayerTwoEndFrequency;
-			PlayerTwoEndFrequency -= FREQUENCY_STEP;
-			PlayerTwoEndFrequency = Mathf.Max (PlayerTwoEndFrequency, 0f);
-			nextPlayerTwoStepTime = Time.fixedTime + STEP_SECONDS;
+		if (Input.GetKeyDown (KeyCode.L)) {
+			IncreaseFrequencyStateP2 ();
+		} else if (Input.GetKeyDown(KeyCode.Semicolon)) {
+			DecreaseFrequencyStateP2 ();
 		}
 
+		// WAVE ANIMATION - PLAYER TWO
 		if (Time.fixedTime < nextPlayerTwoStepTime && PlayerTwoStartFrequency != PlayerTwoEndFrequency) {
 			float percent = (nextPlayerTwoStepTime - Time.fixedTime) / STEP_SECONDS;
 			float value = Mathf.Lerp (PlayerTwoEndFrequency, PlayerTwoStartFrequency, percent);
@@ -99,7 +92,47 @@ public class PlayerController : MonoBehaviour {
 			
 	}
 
-	void newGame() {
+// PLAYER STATE FUNCTIONS
+	public void IncreaseFrequencyStateP1() {
+		// If previous animation is done
+		if (Time.fixedTime > nextPlayerOneStepTime) {
+			PlayerOneStartFrequency = PlayerOneEndFrequency;
+			PlayerOneEndFrequency += FREQUENCY_STEP;
+			nextPlayerOneStepTime = Time.fixedTime + STEP_SECONDS;
+			PlayerOneEndFrequency = Mathf.Min (PlayerOneEndFrequency, 1f);
+		}
+	}
+
+	public void DecreaseFrequencyStateP1() {
+		// If previous animation is done
+		if (Time.fixedTime > nextPlayerOneStepTime) {
+			PlayerOneStartFrequency = PlayerOneEndFrequency;
+			PlayerOneEndFrequency -= FREQUENCY_STEP;
+			nextPlayerOneStepTime = Time.fixedTime + STEP_SECONDS;
+			PlayerOneEndFrequency = Mathf.Max (PlayerOneEndFrequency, 0f);
+		}
+	}
+
+	public void IncreaseFrequencyStateP2() {
+		if (Time.fixedTime > nextPlayerTwoStepTime) {
+			PlayerTwoStartFrequency = PlayerTwoEndFrequency;
+			PlayerTwoEndFrequency += FREQUENCY_STEP;
+			PlayerTwoEndFrequency = Mathf.Min (PlayerTwoEndFrequency, 1f);
+			nextPlayerTwoStepTime = Time.fixedTime + STEP_SECONDS;
+		}
+	}
+
+	public void DecreaseFrequencyStateP2() {
+		if (Time.fixedTime > nextPlayerTwoStepTime) {
+			PlayerTwoStartFrequency = PlayerTwoEndFrequency;
+			PlayerTwoEndFrequency -= FREQUENCY_STEP;
+			PlayerTwoEndFrequency = Mathf.Max (PlayerTwoEndFrequency, 0f);
+			nextPlayerTwoStepTime = Time.fixedTime + STEP_SECONDS;
+		}
+	}
+
+// CHANGE GAME STATE
+	public void newGame() {
 		// Reset Player Variables
 		PlayerOneHealth = DEFAULT_START_HEALTH;
 		PlayerTwoHealth = DEFAULT_START_HEALTH;
@@ -110,5 +143,18 @@ public class PlayerController : MonoBehaviour {
 		PlayerTwoStartFrequency = DEFAULT_START_FREQUENCY;
 		PlayerTwoEndFrequency = DEFAULT_START_FREQUENCY;
 		WaveController.setFrequencies (DEFAULT_START_FREQUENCY, DEFAULT_START_FREQUENCY);
+		respawnBomb ();
+	}
+
+	public void respawnBomb() {
+		GameObject bomb = GameObject.FindGameObjectWithTag ("BOMB");
+		if (bomb != null) {
+			bomb.transform.position = new Vector3 (0f, 5.5f, -2f);
+			bomb.transform.rotation = new Quaternion ();
+
+			Rigidbody2D bombPhysics = bomb.GetComponent<Rigidbody2D> ();
+			bombPhysics.velocity = new Vector2 ();
+			bombPhysics.angularVelocity = 0f;
+		}
 	}
 }
